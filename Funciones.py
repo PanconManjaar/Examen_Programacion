4a300ac15f72ed5164f732c640421124063b2741
Diego <di.marambio@duocuc.cl>
2024-07-11 15:38:41 -0400

    avance del trabajo

--- Funciones.py
+++ Funciones.py
@@ -1,7 +1,7 @@
 #import random 
 #aleatorio = random.randint(menor,mayor)
 import time, csv, os, random, msvcrt
-trabajadores_base = ["Juan Pérez","María García","Carlos López","Ana Martínez","Pedro Rodríguez","Laura Hernández","Miguel Sánchez","Isabel Gómez","Francisco Díaz","Elena Fernández"]
+trabajadores_base = ["Juan Perez","Maria Garcia","Carlos Lopez","Ana Martinez","Pedro Rodriguez","Laura Hernandez","Miguel Sanchez","Isabel Gomez","Francisco Diaz","Elena Fernandez"]
 sueldos_trabajadores =[]
 
 def menu():
@@ -23,7 +23,7 @@ def menu():
         elif opc==3:
             ver_estadisticas()
         elif opc==4:
-            pass
+            reporte_sueldos()
         elif opc==5:
             salir_programa()
 
@@ -31,56 +31,102 @@ def menu():
 def Asignar_sueldos():
     os.system('cls')
     print("ASIGNAR SUELDOS ALEATORIOS")
+    time.sleep(3)
     for x in range(10):
         Sueldos_aleatorios = random.randint(300000,2500000)
+        desc_salud = Sueldos_aleatorios * 0.07
+        desc_afp = Sueldos_aleatorios * 0.12
+        sueldo_liquido = Sueldos_aleatorios - (desc_afp + desc_salud)
+
         sueldo_trabajador= {"nombre":trabajadores_base[x],
-                            "sueldo":Sueldos_aleatorios}
+                            "sueldo":Sueldos_aleatorios,
+                            "descuento_salud":desc_salud,
+                            "descuento_AFP":desc_afp,
+                            "sueldo_liquido":sueldo_liquido}
         
 
 
 
         sueldos_trabajadores.append(sueldo_trabajador)
-    print("Se an generado los sueldos...")
+    print("Se han generado los sueldos...")
     time.sleep(3)
 
 def Clasificar_sueldos():
-    print("CLASIFICACION SUELDOS")
-    print()
-    print("Sueldos menores a $800.000") 
-    print("Nombre empleado   Sueldo")
-    for tra in sueldos_trabajadores:
-        if tra["sueldo"]<800000: 
-           print(f"{tra["nombre"]}       {tra["sueldo"]}")
-    time.sleep(10)
+    os.system('cls')
+    if not sueldos_trabajadores:
+        print("No se han generado los sueldos...")
+    else:
 
-    print()
-    print("Sueldos entre $800.000 y $2.000.000") 
-    print("Nombre empleado   Sueldo")
+        print("CLASIFICACION SUELDOS")
+        print()
+        print("Sueldos menores a $800.000") 
+        print("Nombre empleado   Sueldo")
+        for tra in sueldos_trabajadores:
+            if tra["sueldo"]<800000: 
+                print(f"{tra["nombre"]}       {tra["sueldo"]}")
+        time.sleep(10)
 
-    for tra in sueldos_trabajadores:
-        if tra["sueldo"]>800000 and tra["sueldo"]<2000000:
-           print(f"{tra["nombre"]}       {tra["sueldo"]}")
-    time.sleep(10)
+        print()
+        print("Sueldos entre $800.000 y $2.000.000") 
+        print("Nombre empleado   Sueldo")
 
-    print()
-    print("Sueldos superiores a $2.000.000") 
-    print("Nombre empleado   Sueldo")
+        for tra in sueldos_trabajadores:
+            if tra["sueldo"]>800000 and tra["sueldo"]<2000000:
+                print(f"{tra["nombre"]}       {tra["sueldo"]}")
+        time.sleep(10)
 
-    for tra in sueldos_trabajadores:
-        if tra["sueldo"]>2000000:
-           print(f"{tra["nombre"]}       {tra["sueldo"]}")
-    time.sleep(10)
-    
+        print()
+        print("Sueldos superiores a $2.000.000") 
+        print("Nombre empleado   Sueldo")
+
+        for tra in sueldos_trabajadores:
+            if tra["sueldo"]>2000000:
+                print(f"{tra["nombre"]}       {tra["sueldo"]}")
+        time.sleep(10)
+        
 def ver_estadisticas():
-    print("ESTADISTICAS SUELDOS")
-    print()
+    if not sueldos_trabajadores:
+        print("No se han generado los sueldos...")
+    else:
+        print("ESTADISTICAS SUELDOS")
+        print()
+        print()
+        print()
+        print()
+        for pro in sueldos_trabajadores:
+            promedio = promedio + pro["sueldo"]
+            promedio_final = promedio /10
+        print(f"El promedio de los sueldos es: {promedio_final}")
+        print()
+        for mg in sueldos_trabajadores:
+            pass
 
+
+
+def reporte_sueldos():
+    if not sueldos_trabajadores:
+        print("No se han generado los sueldos...")
+    else:
+
+        print("REPORTE DE SUELDOS")
+
+        nombre_archivo = input("Ingrese nombre archivo: ")+".csv"
+        with open(nombre_archivo,"w",newline="") as archivo:
+            escritor = csv.DictWriter(archivo,["nombre","sueldo","descuento_salud","descuento_AFP","sueldo_liquido"])
+            escritor.writerows(sueldos_trabajadores)
+
+    
+        
 
 
 
 
 
 def salir_programa():
+    print("Finalizando programa...")
+    print("Desarrollado por ")
+    print("RUT ")
+    exit()
     
      
        
